be33c3d8262d0d1c3966240c296a9f50b5da7b1c
James Chartrand <jc.chartrand@gmail.com>
2020-09-02 17:57:55 -0400

    changed solr core

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const SOLR_URL = "http://206.167.181.124:8983/solr/CanLink-new-2/";
+export const SOLR_URL = "http://206.167.181.124:8983/solr/CanLink-new-3/";
 export const SOLR_RECORD_URL = `${SOLR_URL}get?ids=http://canlink.library.ualberta.ca/thesis/`
 export const SOLR_QUERY_URL = `${SOLR_URL}select?q=`
 export const FACET_QUERY = "&json.facet={universities: {type:terms,field:institution_str,limit:100}, subjects: {type:terms,field:subject,limit:100}, degrees: {type:terms,field:degree,limit:100}, languages: {type:terms,field:lang_str,limit:100}, years: {type:terms,field:year,limit:200}}";
