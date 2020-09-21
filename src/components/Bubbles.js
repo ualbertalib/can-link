4db4fcd374414f6bb7ea90cf8790f809785bd821
James Chartrand <jc.chartrand@gmail.com>
2020-09-21 07:36:32 -0400

    remove bubble legend

--- src/components/Bubbles.js
+++ src/components/Bubbles.js
@@ -14,15 +14,15 @@ export default function Bubbles({values}) {
        
       <BubbleChart
         graph= {{
-          zoom: 1.1,
-          offsetX: 0.05,
+          zoom: 0.88,
+          offsetX: 0.00,
           offsetY: -0.01,
         }}
         width={"900"}
         height={"900"}
         padding={4} // optional value, number that set the padding between bubbles
-        showLegend={true} // optional value, pass false to disable the legend.
-        legendPercentage={25} // number that represent the % of with that legend going to use.
+        showLegend={false} // optional value, pass false to disable the legend.
+        legendPercentage={0} // number that represent the % of with that legend going to use.
         
         //Custom bubble/legend click functions such as searching using the label, redirecting to other page
         bubbleClickFunc={bubbleClick}
