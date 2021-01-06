6cfd78eb6edd5caabfe367c5c7a9555d07879d4a
danydvd <danoosh@ualberta.ca>
2021-01-06 12:04:07 -0700

    modify About

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
-import  Toolbar from '@material-ui/core/ToolBar';
+import  Toolbar from '@material-ui/core/Toolbar';
 import { makeStyles } from '@material-ui/core/styles';
 
 import AboutDialog from './AboutDialog';
